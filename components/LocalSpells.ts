8a2fb5e5068f159bdd13cf16218889c5b7d821f4
Thomas van Rossum <thomas_vr@hotmail.nl>
2024-01-08 21:33:53 +0100

    Added cutting words

--- components/LocalSpells.ts
+++ components/LocalSpells.ts
@@ -61,7 +61,23 @@ const LocalSpells: SpellData[] = [
     higher_level: [
       "Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level.",
     ],
-    duration: "Concentration, up to 1 minute",
+    duration: "10 minutes",
+    casting_time: "bonus action",
+    school: { name: "" },
+  },
+  {
+    error: "",
+    name: "Cutting Words",
+    desc: [
+      `When a creature that you can see within 60 feet of you makes an attack roll, an ability check, or a damage roll, you can use your reaction to expend one of your uses of Bardic Inspiration, rolling a Bardic Inspiration die and subtracting the number rolled from the creature’s roll. You can choose to use this feature after the creature makes its roll, but before the DM determines whether the attack roll or ability check succeeds or fails, or before the creature deals its damage. The creature is immune if it can’t hear you or if it’s immune to being charmed.`,
+    ],
+    level: "Trait",
+    components: ["None"],
+    range: "60 feet",
+    higher_level: [
+      "Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level.",
+    ],
+    duration: "Instantaneous",
     casting_time: "bonus action",
     school: { name: "" },
   },
